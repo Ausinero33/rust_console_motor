0f4c0f3900187150db4fb355a0f969fe3b9f2705
GonzaloBurgos <46650297+Ausinero33@users.noreply.github.com>
2022-03-07 11:01:21 +0100

    Delete target directory

--- target/debug/deps/log-f9639f24ab4fa829.d
+++
@@ -1,7 +0,0 @@
-/home/gonzalo/repos/rust_console_motor/target/debug/deps/log-f9639f24ab4fa829.rmeta: /home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.14/src/lib.rs /home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.14/src/macros.rs /home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.14/src/serde.rs
-
-/home/gonzalo/repos/rust_console_motor/target/debug/deps/log-f9639f24ab4fa829.d: /home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.14/src/lib.rs /home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.14/src/macros.rs /home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.14/src/serde.rs
-
-/home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.14/src/lib.rs:
-/home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.14/src/macros.rs:
-/home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.14/src/serde.rs:
--- target/debug/deps/signal_hook_registry-62592eaf8d4e0375.d
+++
@@ -1,6 +0,0 @@
-/home/gonzalo/repos/rust_console_motor/target/debug/deps/signal_hook_registry-62592eaf8d4e0375.rmeta: /home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/signal-hook-registry-1.4.0/src/lib.rs /home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/signal-hook-registry-1.4.0/src/half_lock.rs
-
-/home/gonzalo/repos/rust_console_motor/target/debug/deps/signal_hook_registry-62592eaf8d4e0375.d: /home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/signal-hook-registry-1.4.0/src/lib.rs /home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/signal-hook-registry-1.4.0/src/half_lock.rs
-
-/home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/signal-hook-registry-1.4.0/src/lib.rs:
-/home/gonzalo/.cargo/registry/src/github.com-1ecc6299db9ec823/signal-hook-registry-1.4.0/src/half_lock.rs:
